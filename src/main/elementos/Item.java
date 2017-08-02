9bcb27a8120665d84c2df46447181cac6d03f454
Redson Farias <redson.filho@ccc.ufcg.edu.br>
2017-08-02 16:55:49 -0700

    primeiro_commit

---
+++ src/main/elementos/Item.java
@@ -0,0 +1,51 @@
+package main.elementos;
+
+/**
+ * @author Redson
+ *
+ */
+
+public abstract class Item {
+	
+	protected String nome;
+	protected double valor;
+	protected boolean status;
+	
+	public Item(String nome, double valor, boolean status) {
+		this.nome = nome;
+		this.valor = valor;
+		this.status = false;
+	}
+	
+	public String estado() {
+		if (status == true) 
+			return "Emprestado";
+		else 
+			return "Nao emprestado";
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	public boolean getStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	
+}
---
+++ src/main/elementos/JogoEletronico.java
@@ -0,0 +1,61 @@
+package main.elementos;
+
+/**
+ * @author Redson
+ *
+ */
+
+public class JogoEletronico extends Item{
+
+	private String plataforma;
+	
+	public JogoEletronico(String nome, double valor, boolean status, String plataforma) {
+		super(nome, valor, status);
+		this.plataforma = plataforma;
+	}
+
+	public String getPlataforma() {
+		return plataforma;
+	}
+
+	public void setPlataforma(String plataforma) {
+		this.plataforma = plataforma;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((plataforma == null) ? 0 : plataforma.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		JogoEletronico other = (JogoEletronico) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (plataforma == null) {
+			if (other.plataforma != null)
+				return false;
+		} else if (!plataforma.equals(other.plataforma))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "JOGO ELETRONICO: " + nome + ", R$" + valor + ", " + estado() + ", " + plataforma;
+	}
+	
+}
