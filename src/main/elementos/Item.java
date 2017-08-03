abd23ece820f7db33ca38aa07bd290be27237df9
matheusthiago95 <matheus_thiagothebest@hotmail.com>
2017-08-02 22:50:20 -0300

    Adicionei a lista de itens para o usuário, como também os métodos de adicionar, remover e atulizar, como o equals e hashcode na classe abstrata itens

--- src/main/elementos/Item.java
+++ src/main/elementos/Item.java
@@ -48,4 +48,34 @@ public abstract class Item {
 		this.status = status;
 	}
 	
+	/**
+	 * hashcode e equals que compara se doi objetos itens tem nomes iguais.
+	 * @author matheus
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+	
+	
 }
