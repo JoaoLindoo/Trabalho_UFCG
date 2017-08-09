59c47439f3e6e1efd50f504af8381027b6f0db49
matheusthiago95 <matheus_thiagothebest@hotmail.com>
2017-08-09 19:47:25 -0300

    Criação da classe teste SistemaControllerTest

---
+++ testes/main/testes/SistemaControllerTest.java
@@ -0,0 +1,104 @@
+package main.testes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SistemaControllerTest {
+
+	@Test
+	public void testAdicionar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetInfoUsuario() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoverUsuario() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAtualizarUsuario() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRecuperar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCadastrarEletronico() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetInfoItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCadastrarJogoTabuleiro() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdicionarPecaPerdida() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCadastrarBluRaySerie() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCadastrarBluRayFilme() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCadastrarBluRayShow() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdicionarBluRay() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoverItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAtualizarItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPesquisarDetalhesItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRegistrarEmprestimo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAlocaItemEmprestado() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDevolverItem() {
+		fail("Not yet implemented");
+	}
+
+}
